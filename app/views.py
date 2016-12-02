9a5e7258bd6c4bcd2f778034316dfe66e8662da8
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-02 11:11:00 +0545

    GET API created from Vue to flask route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 
-from flask import render_template
+from flask import render_template, jsonify
 from app import app, db
 from .models import User
+import json
 
 @app.route('/')
 def index():
@@ -10,7 +11,12 @@ def index():
 @app.route('/api/name', methods=['GET'])
 def get_name():
     users = User.query.all()
+    names = []
+    for user in users:
+        names.append(user.name)
+    print json.dumps(names)
+    return json.dumps(names)
 
-@app.route('/api/name/', methods=['POST'])
-def post_name():
-    User(name=name)
+# @app.route('/api/name/', methods=['POST'])
+# def post_name():
+#     User(User.name=name)
