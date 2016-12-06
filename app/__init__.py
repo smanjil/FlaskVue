ab508a88b434fdd521b40564dc561eda78736b4d
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-06 16:56:18 +0545

    Token authorization through vue webpack

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 app.config.from_object('config')
 
--- app/views.py
+++ app/views.py
@@ -1,20 +1,45 @@
 
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, g, request
+from flask_httpauth import HTTPTokenAuth
 from app import app, db
 from .models import User
 import json
 
+from flask_cors import cross_origin
+
+auth = HTTPTokenAuth(scheme='Token')
+
+tokens = {
+    'secret-token-1': 'John',
+    'secret-token-2': 'Susan'
+}
+
+@auth.verify_token
+def verify_token(token):
+    print token
+    if token in tokens:
+        g.current_user = tokens[token]
+        return True
+    return False
+
 @app.route('/')
+@cross_origin()
+@auth.login_required
 def index():
-    return render_template('show_form.html',
-                           title='Vue Request')
+    # print request.headers
+    # print 'Hello %s!' % g.current_user
+    return None
 
 @app.route('/api/name', methods=['GET'])
+@cross_origin()
+@auth.login_required
 def get_name():
+    # print 'here'
     users = User.query.all()
     names = []
     for user in users:
         names.append(user.name)
+    print names
     return json.dumps(names)
 
 @app.route('/api/name/<name>', methods=['POST'])
